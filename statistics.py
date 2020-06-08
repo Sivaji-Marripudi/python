823ce01e8db7e72cc0239559700b0045bdec2ca0
Sivaji Chowdary <60888653+sivaji-463@users.noreply.github.com>
2020-06-08 21:49:17 +0530

    Create statistics.py

---
+++ statistics.py
@@ -0,0 +1,7 @@
+# Statistics in python
+# How to find mean,median,mode using python 
+from statistics import mean,mode,median
+print(median([1,2,3,4,5,6]))
+print(mode([1,2,3,4,3,2,3,3,3,3,5,6,4,5,6,4,6]))
+print(mean([1,4,5,6,4,6,4,7,5,9,10,35,43]))
+
