b23384c38037635a4f7d899b2f33357beebc57ef
Sivaji Chowdary <60888653+sivaji-463@users.noreply.github.com>
2020-06-08 21:58:30 +0530

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,14 @@
+'''
+Tuple is a collection of heterogeneous data items
+tuple is immutable i.e not changble
+'''
+# example
+tuple = (1,2,"hai",5.1)
+print(tuple)
+# access the tuple using index
+print(tuple[1]) # 2
+# loop through tuple
+for i in tuple:
+  print(i)
+# coverts tuple to list
+print(list(tuple))
