608b683edd3050360f2d5a7eb108e56163cee7bf
Sivaji Chowdary <60888653+sivaji-463@users.noreply.github.com>
2020-06-08 22:07:20 +0530

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,13 @@
+# string is a group of characters 
+# example
+string = "sivaji"
+print(string)
+# multiline string
+str = ''' Python is a popular programming language. It was created by Guido van Rossum, and released in 1991.
+It is used for:
+web development (server-side),
+software development,
+mathematics,
+system scripting. 
+'''
+print(str)
