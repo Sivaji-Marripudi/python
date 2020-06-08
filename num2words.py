641ce921ce2198b54d24b9ddb7706938f48c7846
Sivaji Chowdary <60888653+sivaji-463@users.noreply.github.com>
2020-06-08 21:19:55 +0530

    Create num2words.py

---
+++ num2words.py
@@ -0,0 +1,16 @@
+
+from num2words import num2words
+dict = {}
+list = []
+for i in range(1,11):
+    #dict[i] = num2words(i)
+    list.append(num2words(i))
+print(list)
+
+from num2words import num2words
+from collections import *
+list = []
+for i in range(1,11):
+    j = num2words(i)
+    list.append(j)
+print(Counter(list))
