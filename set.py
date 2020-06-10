086eb6dd18ad6c3ab6177587b39cc7da98956e14
sivaji chowdary <sivajimarripudi98@gmail.com>
2020-06-10 21:41:36 +0530

    created set.py

---
+++ set.py
@@ -0,0 +1,11 @@
+# set is a collection of data items,which is orderable and immutable
+set = {1,2,3,4,2,4,5,67,3,2,4}
+print(set)
+set.add(100)
+#print(set)
+set.add(101)
+#print(set)
+set.add(75)
+print(set)
+set.clear()
+print(set)
