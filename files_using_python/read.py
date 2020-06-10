fefdcbddaad2fade10b52bed8ff45f65cda52207
sivaji chowdary <sivajimarripudi98@gmail.com>
2020-06-10 10:32:02 +0530

    updated read.py

--- files_using_python/read.py
+++ files_using_python/read.py
@@ -1,3 +1,15 @@
 with open("data.txt","r") as f:
     data = f.read()
 print(data)
+# using loops
+for i in data:
+    print(i)
+##### another method for open the file  ######
+data = open("data.txt","r")
+print(data.read())
+# read only 5 lines
+print(data.read(5))
+# only one line
+print(data.read(1))
+#.......or
+print(data.readline())
