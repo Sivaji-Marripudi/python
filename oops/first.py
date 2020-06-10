24c67d7c48cd93bed85084f60c2fdb9ce1d32679
sivaji chowdary <sivajimarripudi98@gmail.com>
2020-06-10 22:22:56 +0530

    oops created

---
+++ oops/first.py
@@ -0,0 +1,8 @@
+class persons:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+p1 = persons("husband",21)
+p2 = persons("wife",20)
+print(p1.name)
+print(p2.age)
