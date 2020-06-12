5d0d6c6f34b5a4220184b9b5a14e233fe5ef1768
sivaji chowdary <sivajimarripudi98@gmail.com>
2020-06-12 13:52:24 +0530

    'updated'

--- w3resources/list.py
+++ w3resources/list.py
@@ -4,12 +4,14 @@ sum = 0
 for i in li:
     sum += i
 print(sum)
+
 # multiplies all the itmes in list
 li = [5,6,7,3,5,6,7]
 mul = 1
 for i in li:
     mul *= i
 print(mul)
+
 # to get the largest number in list
 li = [4,5,6,2,3,24,4,11]
 lar = li[0]
@@ -21,6 +23,7 @@ for i in li:
 print(lar)
 # another method to get largest item in list
 print(max(li)) # 'max' is a built_in method in list
+
 #to count the number of strings where the string length is 2 or more and the first and last character are same from a given list of strings
 #Sample List : ['abc', 'xyz', 'aba', '1221'],Expected Result : 2
 li = ['abc','xyz','aba','1221']
@@ -29,8 +32,26 @@ for i in li:
         print(i)
     else:
         pass
+
 #Write a Python program to get a list, sorted in increasing order by the last element in each tuple from a given list of non-empty tuples.
 #Sample List : [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)],Expected Result : [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
+
+# remove duplicates in list
+li = [4,5,6,3,4,5,7,23,3,4,1,1,1,7,6,5,8,9]
+res = []
+for i in li:
+    if i not in res:
+        res.append(i)
+print(res)
+############################### ANOTHER METHOD 
+from collections import Counter
+new = []
+a = Counter(li)
+for i,j in a.items():
+    new.append(i)
+print(new)
+########## ANOTHER METHOD 
+print(set(li))
 
 # check list is empty or not
 li = []
@@ -38,6 +59,7 @@ if not li:
     print('list is empty')
 else:
     print('list is not empty')
+
 #  Write a Python function that takes two lists and returns True if they have at least one common member.
 def lists(a,b):
     for i in a:
@@ -48,3 +70,13 @@ def lists(a,b):
         return 'not found'
 res = lists([11,12,14,34,22,65,77],[99,56,87,33,99,10,3,6,77])
 print(res)
+
+# Write a Python program to find the list of words that are longer than n from a given list of words.
+def check(a,n):
+    n = int(input('enter the n value : '))
+    res = []
+    for i in a:
+        if len(i) > n:
+            res.append(i)
+    print(res)
+check(['hyderabad','bangalore','chennai','kolkata','ongole','mumbai','pune','noida'],'l')
