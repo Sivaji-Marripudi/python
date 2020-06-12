e0cfd2b3bed1bdfd3c553f9c26c0a93c7c54855f
sivaji chowdary <sivajimarripudi98@gmail.com>
2020-06-12 10:38:44 +0530

    updated

--- CSV/write.py
+++ CSV/write.py
@@ -5,3 +5,9 @@ with open("data.csv","r") as csvfile:
         csv_writer = csv.writer(new_csvfile,delimiter = "-")
         for data in csv_file:
             csv_writer.writerow(data)
+# read new_data.csv file
+'''
+with open('new_data.csv','r') as csvfile:
+    csv_file = csv.reader(csvfile)
+    for data in csv_file:
+        print(data)'''
