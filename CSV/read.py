a8d1824b897dc10b8ac71ff97db53b0c16d70a03
sivaji chowdary <sivajimarripudi98@gmail.com>
2020-06-22 16:35:03 +0530

    updated

--- CSV/read.py
+++ CSV/read.py
@@ -1,6 +1,14 @@
+# read csv file
 import csv
 with open ('data.csv','r') as csv_file:
     data = csv.reader(csv_file)
     next(data)
     for i in data:
         print(i)
+
+# read specific row in csv file and print its content
+import csv
+with open('data.csv','r') as csv_file:
+    data = csv.DictReader(csv_file)
+    for i in data:
+        print(i['beat'],i['grid'])
